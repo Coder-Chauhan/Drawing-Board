08641a9a06c94848d838fa76a4551563b88db949
Bhanu PS Chauhan <75789100+Coder-Chauhan@users.noreply.github.com>
2022-02-07 03:09:13 +0530

    v1

---
+++ app.js
@@ -0,0 +1,80 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const decreaseSize = document.getElementById('decrease');
+const increaseSize = document.getElementById('increase');
+const displaySize = document.getElementById('size');
+const penColor = document.getElementById('color');
+const clearCanvas = document.getElementById('clear');
+
+let size = 20;
+let color = penColor.value;
+let x;
+let y;
+let isPressed = false;
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true;
+    x = e.offsetX;
+    y = e.offsetY;
+})
+
+canvas.addEventListener('mouseup', () => {
+    isPressed = false;
+    x = undefined;
+    y = undefined;
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
+    }
+})
+
+decreaseSize.addEventListener('click', () => {
+    if (size > 5) {
+        size -= 5;
+        displaySize.innerHTML = size;
+    }
+})
+
+// Changing Size
+increaseSize.addEventListener('click', () => {
+    if (size < 20) {
+        size += 5;
+        displaySize.innerHTML = size;
+    }
+})
+
+// Changing color
+canvas.addEventListener('mousedown', () => {
+    color = penColor.value;
+    console.log(color);
+});
+
+// Clear Canvas
+clearCanvas.addEventListener('click', () => {
+    ctx.clearRect(0,0,1200,600);
+})
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
+}
